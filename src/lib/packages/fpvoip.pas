348f910b4de8b9dbb6285b2a73f355b3b175e07c
Ido Kanner <idok@linesip.com>
2011-05-06 22:05:53 +0300

    Created package for the library

---
+++ src/lib/packages/fpvoip.pas
@@ -0,0 +1,14 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit fpvoip; 
+
+interface
+
+uses
+  fpsocket_api;
+
+implementation
+
+end.
